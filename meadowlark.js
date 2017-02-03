062b5d82583397f80fc3ede1efda1a853a2091e6
nidjel <nidjel87@gmail.com>
2017-02-03 15:19:31 +0500

    add weather widget

--- meadowlark.js
+++ meadowlark.js
@@ -8,14 +8,27 @@ app.set('view engine', 'handlebars');
 
 var fortune = require('./lib/fortune.js');
 
+app.disable('x-powered-by');
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
   res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weatherContext = getWeatherData();
   next();
 });
+
+/*app.get('/headers', function(req, res) {
+  res.set('Content-type', 'text/plain');
+  var s = '';
+  for (var header in req.headers) {
+    s += header + ': ' + req.headers[header] + '\n';
+  }
+  res.send(s);
+});*/
 
 app.get('/', function(req, res) {
   res.render('home');
@@ -28,7 +41,7 @@ app.get('/about', function(req, res) {
   });
 });
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
   res.status(404);
   res.render('404');
 });
@@ -42,3 +55,31 @@ app.use(function(err, req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Express is running on ' + app.get('port'));
 });
+
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Портленд',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Сплошная облачность',
+        temp: '54.1 F (12.3 C)'
+      },
+      {
+        name: 'Бенд',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Малооблачно',
+        temp: '55.0 F (12.8 C)'
+      },
+      {
+        name: 'Манзанита',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Небольшой дождь',
+        temp: '55.0 F (12.8 C)',
+      }
+    ]
+  };
+}
