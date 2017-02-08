efa3ce100fe4a19462eeac4d19b601e30982058c
nidjel <nidjel87@gmail.com>
2017-02-08 17:48:32 +0500

    add выбор валюты тура

--- meadowlark.js
+++ meadowlark.js
@@ -271,19 +271,40 @@ app.get('/data/nursery-rhyme', function(req, res) {
   });
 });
 
+app.get('/set-currency/:currency', function(req, res) {
+  req.session.currency = req.params.currency;
+  return res.redirect(303, '/vacations');
+});
+
+function convertFromUSD(value, currency) {
+  switch(currency) {
+    case 'USD': return value * 1;
+    case 'GBP': return value * 0.6;
+    case 'BTC': return value * 0.00237;
+    default: return NaN;
+  }
+};
+
 app.get('/vacations',  function(req, res) {
   Vacation.find({available: true}, function(err, vacations) {
+    var currency = req.session.currency || 'USD';
     var context = {
+      currentcy: currency,
       vacations: vacations.map(function(vacation) {
         return {
           sku: vacation.sku,
           name: vacation.name,
           description: vacation.description,
-          price: vacation.getDisplayPrice(),
+          price: convertFromUSD(vacation.priceInCents/100, currency),
           inSeason: vacation.inSeason
-        }
+        };
       })
     };
+    switch(currency) {
+      case 'USD': context.currencyUSD = 'selected'; break;
+      case 'GBP': context.currencyGBP = 'selected'; break;
+      case 'BTC': context.currencyBTC = 'selected'; break;
+    }
     res.render('vacations', context);
   });
 });
